f97d5f7ad21b3ac8c3ff2867baccc9f62023029d
dailuaine0521 <antoniook27@gmail.com>
2026-09-18 00:49:12 +0900

    Bump cache for OCR and analysis update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "private-budget-v4";
+const CACHE = "private-budget-v5";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./style.css", "./app.js"];
 
 self.addEventListener("install", event => {
