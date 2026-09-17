e1d1b68bedbc13a918ff72ed28b12a66ecd87423
dailuaine0521 <antoniook27@gmail.com>
2026-09-17 15:58:55 +0900

    Refresh service worker cache for hardened app

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "private-budget-v1";
-const ASSETS = ["./", "./index.html", "./manifest.json"];
+const CACHE = "private-budget-v2";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./style.css", "./app.js"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
@@ -7,7 +7,10 @@ self.addEventListener("install", event => {
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", event => {
