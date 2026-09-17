0505aab53b54e331a479c0b49e7d6b98da7a2174
dailuaine0521 <antoniook27@gmail.com>
2026-09-18 01:03:58 +0900

    Bump cache for smart subcategory update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "private-budget-v6";
+const CACHE = "private-budget-v7";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./style.css", "./app.js"];
 
 self.addEventListener("install", event => {
