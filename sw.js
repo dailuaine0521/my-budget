1e0c3aa0eb7eef52a59d7282afd286d6d7dc3700
dailuaine0521 <antoniook27@gmail.com>
2026-09-17 23:28:20 +0900

    Bump cache after auto-lock fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "private-budget-v3";
+const CACHE = "private-budget-v4";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./style.css", "./app.js"];
 
 self.addEventListener("install", event => {
