2b39bfad393ebf05216ad9c3ad3e09008e466290
dailuaine0521 <antoniook27@gmail.com>
2026-09-17 23:19:35 +0900

    Refresh service worker cache for budget app update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "private-budget-v2";
+const CACHE = "private-budget-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./style.css", "./app.js"];
 
 self.addEventListener("install", event => {
