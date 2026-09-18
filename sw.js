1a886cf8d8ec33c2d8bf7e924a2495f07b1bf910
dailuaine0521 <antoniook27@gmail.com>
2026-09-18 09:11:03 +0900

    Refresh cache for nested expense details

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "private-budget-v9";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./style.css", "./app-v9.js"];
+const CACHE = "private-budget-v10";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./style.css", "./app-v10.js"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
